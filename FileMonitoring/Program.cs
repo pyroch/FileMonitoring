e20aa29023900a760f332de7123a2ad977f3379e
pyroch <pyrodd@gmail.com>
2017-05-28 22:27:30 +0300

    bak_dir - const, *.bak + numofbak

--- FileMonitoring/Program.cs
+++ FileMonitoring/Program.cs
@@ -7,30 +7,47 @@ namespace FileMonitoring
 	class MainClass
 	{
 		const string dir = "D:\\Documents\\C";
+		const string bak_dir = "\\copy\\";
 
 		static void CheckChange(FileInfo[] files)
 		{
 			bool isChanged = false;
 
-			if (!Directory.Exists(dir + "\\copy")) Directory.CreateDirectory(dir + "\\copy");
+			if (!Directory.Exists(dir + bak_dir)) Directory.CreateDirectory(dir + bak_dir);
 			while (true)
 			{
 				DirectoryInfo info2 = new DirectoryInfo(dir);
 				FileInfo[] files2 = info2.GetFiles();
 
 				for (int i = 0; i < files.Length; i++)
+				{
 					if (files[i].LastWriteTime != files2[i].LastWriteTime)
 					{
-						files2[i].CopyTo(dir + "\\copy\\" + files2[i].Name, true);
+						if (File.Exists(dir + bak_dir + files2[i].Name + ".bak"))
+						{
+							for (int j = 0; j <= int.MaxValue; j++)
+							{
+								if (!File.Exists(dir + bak_dir + files2[i].Name + ".bak" + j))
+								{
+									files2[i].CopyTo(dir + bak_dir + files2[i].Name + ".bak" + j, true);
+									break;
+								}
+							}
+						}
+						else
+						{
+							files2[i].CopyTo(dir + bak_dir + files2[i].Name + ".bak", true);
+						}
 						isChanged = true;
 						break;
 					}
+				}
 				if (isChanged) break;
 
 				Thread.Sleep(50);
 			}
-					
 		}
+
 		public static void Main(string[] args)
 		{
 			while (true)
