a0d0a8e59e58fcdc17a3bda266e0bf03a4864efc
pyroch <pyrodd@gmail.com>
2017-06-01 13:18:28 +0300

    Interface++

--- FileMonitoring/Program.cs
+++ FileMonitoring/Program.cs
@@ -8,15 +8,50 @@ namespace FileMonitoring
 	{
 		static bool flag_OW = false; // Flag for owerwriting
 
-		const string dir = "C:\\Test\\"; // Directory which is checked
-		const string bak_dir = "\\copy\\"; // Directory of backup files
+		static string dir; // Directory which is checked
+		static string bak_dir; // Directory of backup files
 		const string prefix = ".bak"; // Prefix for backup files
+
+
+		static void Init()
+		{
+			Console.WriteLine("Choose a new directory? (N - no)");
+			if (Console.ReadLine() == "N" && File.Exists(@"./settings"))
+			{
+				using (StreamReader sr = new StreamReader(@"./settings", System.Text.Encoding.Default))
+				{
+					dir = sr.ReadLine();
+					bak_dir = sr.ReadLine();
+				}
+			}
+			else
+			{
+				Console.WriteLine("Type directory what to be monitored");
+				dir = Console.ReadLine();
+				Console.WriteLine("Type directory for backup files");
+				bak_dir = Console.ReadLine();
+				bak_dir += @"\";
+
+				Console.WriteLine("Add owerwrite flag? (Y - yes)");
+				if (Console.ReadLine() == "Y")
+					flag_OW = true;
+				else
+					flag_OW = false;
+					
+
+				using (StreamWriter sw = new StreamWriter(@"./settings", false, System.Text.Encoding.Default))
+				{
+					sw.WriteLine(dir);
+					sw.WriteLine(bak_dir);
+				}
+			}
+		}
 
 		static void CheckChange(FileInfo[] files)
 		{
 			bool isChanged = false;
 
-			if (!Directory.Exists(dir + bak_dir)) Directory.CreateDirectory(dir + bak_dir);  // Checking backup directory
+			if (!Directory.Exists(bak_dir)) Directory.CreateDirectory(bak_dir);  // Checking backup directory
 			while (true)
 			{
 				DirectoryInfo info2 = new DirectoryInfo(dir);
@@ -26,20 +61,21 @@ namespace FileMonitoring
 				{
 					if (files[i].LastWriteTime != files2[i].LastWriteTime) // If new file != old file ->>
 					{
-						if (File.Exists(dir + bak_dir + files2[i].Name + prefix) && !flag_OW) // If file exist, and has no flag OW, starting for cycle where adding num of file in prefix
+						string fullName = bak_dir + files2[i].Name + prefix;
+						if (File.Exists(fullName) && !flag_OW) // If file exist, and has no flag OW, starting for cycle where adding num of file in prefix
 						{
 							for (int j = 1; j <= int.MaxValue; j++)
 							{
-								if (!File.Exists(dir + bak_dir + files2[i].Name + prefix + j)) // If file with num not exist, backuping
+								if (!File.Exists(fullName + j)) // If file with num not exist, backuping
 								{
-									files2[i].CopyTo(dir + bak_dir + files2[i].Name + prefix + j, true);
+									files2[i].CopyTo(fullName + j, true);
 									break;
 								}
 							}
 						}
 						else
 						{
-							files2[i].CopyTo(dir + bak_dir + files2[i].Name + prefix, true); // Backuping, if its new file in backup dir
+							files2[i].CopyTo(fullName, true); // Backuping, if its new file in backup dir
 						}
 
 						isChanged = true; // bool for breaking while cycle
@@ -54,6 +90,8 @@ namespace FileMonitoring
 
 		public static void Main(string[] args)
 		{
+			Init();
+
 			foreach (var args_s in args) // If Main receivec argument -OW, bak files will be owerwritten
 				if (args_s == "-OW")
 					flag_OW = true;
